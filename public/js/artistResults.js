92d8f972f91088a9c582cca23a60f2614a37bab2
Rex <roydavis99.rd@gmail.com>
2018-11-26 21:17:47 -0700

    check in for master. header and footer working map working

--- public/js/artistResults.js
+++ public/js/artistResults.js
@@ -1,36 +1,65 @@
-$(document).on("click", "@btn-like", function () {
-//get userid
+function CreateLike(userId, bandId) {
+  try {
+    $.ajax({
+      method: "POST",
+      url: "api/userband/",
+      data: {
+        UserId: userId,
+        BandId: bandId
+      }
+    }).then(function (data) {
+      console.log(data);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+$(document).on("click", "#btn-like", function () {
+  //get userid
   var userid = sessionStorage.getItem("sessionId");
 
-  if(userid === undefined){
+  console.log(userid);
+  if (userid === null) {
     //load login form
-
+    window.location.replace("/login");
     return;
   }
 
-  var bandId = $(this).attr("data-id");
-  
+  //var bandId = $(this).attr("data-id");
+
   var bandName = $(this).attr("data-name");
   var bandImage = $(this).attr("data-image");
   var bandPage = $(this).attr("data-page");
-
-  if (bandId === "") {
-    //create band.
-    console.log("new");
+  try {
     $.ajax({
-      method: "POST",
-      url: "api/band/",
-      data:{
-        name: bandName,
-        image:bandImage,
-        link:bandPage
+      method: "GET",
+      url: "api/band/byname/" + bandName
+    }).then(function (band) {
+      console.log(band);
+      //band exists
+      if (band === null) {
+        $.ajax({
+          method: "POST",
+          url: "api/band/",
+          data: {
+            name: bandName,
+            image: bandImage,
+            link: bandPage
+          }
+        }).then(function (band) {
+          //call for save on like
+          console.log(band);
+          //band exists
+          CreateLike(userid, band.id);
+        });
+      } else {
+        CreateLike(userid, band.id);
       }
-    }).then(function(band){
-      //call for save on like
-    }).catch(function(){
-  
-      console.log("bad log in");
+
     });
+  } catch (error) {
+    console.log(error);
   }
 
 });
